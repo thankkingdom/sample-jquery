fa7a09e8de31d21ce53af9a940eef46585f536ae
thankkingdom <thanksphere@gmail.com>
2019-08-13 18:12:26 +0900

    初期状態

---
+++ src/main/java/com/sample/samplejquery/SampleJqueryApplication.java
@@ -0,0 +1,13 @@
+package com.sample.samplejquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJqueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/samplejquery/SampleJqueryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sample.samplejquery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleJqueryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
